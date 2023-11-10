810020349440b8cea40a6caeff5eb80a4103db16
Tobias Kirschstein <tobias.kirschstein@gmail.com>
2023-11-10 22:04:51 +0100

    Fix nn.Module must have forward() error

--- src/face_parser/bounding_boxes/face_boxes_v2.py
+++ src/face_parser/bounding_boxes/face_boxes_v2.py
@@ -1,5 +1,11 @@
-from face_parser.modules.FaceBoxesV2.faceboxes_detector import FaceBoxesDetector
+from typing import List
+
+import numpy as np
+
+from face_parser.modules.FaceBoxesV2.faceboxes_detector import FaceBoxesDetector, DetectedBBox
 
 
 class FaceBoxesV2(FaceBoxesDetector):
-    pass
+
+    def forward(self, image: np.ndarray, thresh: float = 0.6) -> List[DetectedBBox]:
+        return self.detect(image, thresh=thresh)
